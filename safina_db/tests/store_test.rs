c5ba922ec74bb60286ad4c8291087215e4775d60
3dida <44850191+aallali@users.noreply.github.com>
2024-05-27 18:40:27 +0100

    [fix] : wrong imports + adapt to Mutex wrap : @ Tests

--- safina_db/tests/store_test.rs
+++ safina_db/tests/store_test.rs
@@ -1,85 +1,144 @@
+use once_cell::sync::Lazy;
+use safina_db::{Store, STORAGE_MUTEX};
+use std::sync::Mutex;
+use std::time::{SystemTime, UNIX_EPOCH};
 
-#[path = "../src/kv_store.rs"] mod kv_store;
+#[cfg(test)]
+pub static TEST_STORE: Lazy<Mutex<Store>> = Lazy::new(|| {
+    // Perform any setup needed before tests run
+    let mut store = Store::new();
+    let start = SystemTime::now();
+    let since_the_epoch = start.duration_since(UNIX_EPOCH).expect("Time went backwards");
+    let timestamp = since_the_epoch.as_secs();
+    let db_name = format!("db-test-{}", timestamp);
+ 
+    println!("- Loading test database...");
+    match STORAGE_MUTEX.lock().unwrap().load_file(Some(db_name.as_str())) {
+        Ok(data) => store.data = data,
+        Err(err) => {
+            println!("Invalid: {}", err);
+        }
+    }
+    // Initialize store or perform setup
+    Mutex::new(store)
+});
 
 #[cfg(test)]
 mod tests {
-    use crate::kv_store::Store;
-
+    use super::TEST_STORE;
+    use safina_db::kv_store::KV;
     #[test]
     fn test_insert_new_key() {
-        let mut store = Store::new();
-        let result = store.insert("key1", "value1");
+        let mut store = TEST_STORE.lock().unwrap();
+        let test_data = KV {
+            key: String::from("key1"),
+            value: String::from("value1")
+        };
+        let result = store.insert(&test_data.key, &test_data.value);
         assert!(result.is_ok());
         assert_eq!(store.get("key1").unwrap().value, "value1");
     }
 
     #[test]
     fn test_insert_empty_key() {
-        let mut store = Store::new();
-        let result = store.insert("", "value1");
+        let test_data = KV {
+            key: String::from(""),
+            value: String::from("value2")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        let result = store.insert(&test_data.key, &test_data.value);
         assert!(result.is_ok());
-        assert_eq!(store.get("").unwrap().value, "value1");
+        assert_eq!(store.get("").unwrap().value, test_data.value);
     }
 
     #[test]
     fn test_insert_existing_key() {
-        let mut store = Store::new();
-        store.insert("key1", "value1").unwrap();
-        let result = store.insert("key1", "value2");
+        let test_data = KV {
+            key: String::from("key3"),
+            value: String::from("value3")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        let result = store.insert(&test_data.key, &test_data.value);
         assert!(result.is_err());
-        assert_eq!(result.err(), Some("Key already exists"));
+        assert_eq!(result.err(), Some("Key already exists".to_string()));
     }
 
     #[test]
     fn test_get_existing_key() {
-        let mut store = Store::new();
-        let expected_resp = "value1,value2,value3";
-        store.insert("key1", expected_resp).unwrap();
-        let result = store.get("key1");
+        let test_data = KV {
+            key: String::from("key4"),
+            value: String::from("value4")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+ 
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        let result = store.get(&test_data.key);
         assert!(result.is_ok());
-        assert_eq!(result.unwrap().value, expected_resp);
+        assert_eq!(result.unwrap().value, test_data.value);
     }
 
     #[test]
     fn test_get_non_existing_key() {
-        let mut store = Store::new();
-        let result = store.get("key1");
+        let test_data = KV {
+            key: String::from("key5"),
+            value: String::from("")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        let result = store.get(&test_data.key);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Key not found"));
     }
 
     #[test]
     fn test_update_existing_key() {
-        let mut store = Store::new();
-        store.insert("key1", "value1").unwrap();
-        let result = store.update("key1", "value2");
+        let test_data = KV {
+            key: String::from("key6"),
+            value: String::from("value6")
+        };
+        let test_data_update = KV {
+            key: test_data.key.clone(),
+            value: String::from("value6-updated")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        let result = store.update(&test_data_update.key, &test_data_update.value);
         assert!(result.is_ok());
-        assert_eq!(store.get("key1").unwrap().value, "value2");
+        assert_eq!(store.get(&test_data.key).unwrap().value, test_data_update.value);
     }
 
     #[test]
     fn test_update_non_existing_key() {
-        let mut store = Store::new();
-        let result = store.update("key1", "value1");
+        let test_data_update = KV {
+            key: String::from(""),
+            value: String::from("value7-updated")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.delete(&test_data_update.key);
+        let result = store.update(&test_data_update.key, &test_data_update.value);
         assert!(result.is_err());
-        assert_eq!(result.err(), Some("Key not found"));
+        assert_eq!(result.err(), Some("Key not found".to_string()));
     }
 
     #[test]
     fn test_delete_existing_key() {
-        let mut store = Store::new();
-        store.insert("key1", "value1").unwrap();
-        store.delete("key1");
-        let result = store.get("key1");
+        let test_data = KV {
+            key: String::from("key8-should-be-deleted"),
+            value: String::from("value8-should-be-deleted")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        store.delete(&test_data.key);
+        let result = store.get(&test_data.key);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Key not found"));
     }
 
     #[test]
     fn test_delete_non_existing_key() {
-        let mut store = Store::new();
-        store.delete("key1"); // Should not panic or cause error
-        let result = store.get("key1");
+        let mut store = TEST_STORE.lock().unwrap();
+        store.delete("key-doesnt-exists"); // Should not panic or cause error
+        let result = store.get("key-doesnt-exists");
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Key not found"));
     }
@@ -87,15 +146,19 @@ mod tests {
     // Additional edge cases
     #[test]
     fn test_insert_empty_value() {
-        let mut store = Store::new();
-        let result = store.insert("key", "");
+        let test_data = KV {
+            key: String::from("key9-with-empty-value"),
+            value: String::from("")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        let result = store.insert(&test_data.key, &test_data.value);
         assert!(result.is_ok());
-        assert_eq!(store.get("key").unwrap().value, "");
+        assert_eq!(store.get(&test_data.key).unwrap().value, test_data.value);
     }
 
     #[test]
     fn test_insert_large_key_value() {
-        let mut store = Store::new();
+        let mut store = TEST_STORE.lock().unwrap();
         let large_key = "k".repeat(1000);
         let large_value = "v".repeat(1000);
         let result = store.insert(large_key.clone().as_str(), large_value.clone().as_str());
@@ -105,29 +168,42 @@ mod tests {
 
     #[test]
     fn test_update_empty_value() {
-        let mut store = Store::new();
-        store.insert("key", "value").unwrap();
-        let result = store.update("key", "");
+        let test_data = KV {
+            key: String::from("key10"),
+            value: String::from("value10")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        let result = store.update(&test_data.key, "");
         assert!(result.is_ok());
-        assert_eq!(store.get("key").unwrap().value, "");
+        assert_eq!(store.get(&test_data.key).unwrap().value, "");
     }
 
     #[test]
     fn test_delete_empty_key() {
-        let mut store = Store::new();
-        store.insert("", "value").unwrap();
-        store.delete("");
-        let result = store.get("");
+        let test_data = KV {
+            key: String::from(""),
+            value: String::from("value11-with-empty-key")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        store.delete(&test_data.key);
+        let result = store.get(&test_data.key);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Key not found"));
     }
 
     #[test]
     fn test_delete_multiple_times() {
-        let mut store = Store::new();
-        store.insert("key", "value").unwrap();
-        store.delete("key");
-        let result = store.get("key");
+        let test_data = KV {
+            key: String::from("key-12"),
+            value: String::from("value12")
+        };
+        let mut store = TEST_STORE.lock().unwrap();
+        store.insert(&test_data.key, &test_data.value).unwrap();
+        store.delete(&test_data.key);
+        store.delete(&test_data.key);
+        let result = store.get(&test_data.key);
         assert!(result.is_err());
         assert_eq!(result.err(), Some("Key not found"));
     }
