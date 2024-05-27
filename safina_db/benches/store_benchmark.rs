0378862c95134b977bfcac939f87a00d636ab2a4
3dida <44850191+aallali@users.noreply.github.com>
2024-05-27 18:40:44 +0100

    [fix] : wrong imports + adapt to Mutex wrap : @ Benchmarks

--- safina_db/benches/store_benchmark.rs
+++ safina_db/benches/store_benchmark.rs
@@ -1,61 +1,79 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-#[path = "../src/kv_store.rs"] mod kv_store;
-use kv_store::Store;
+use safina_db::{Store, STORAGE_MUTEX};
+use std::time::{SystemTime, UNIX_EPOCH};
 
-pub fn bench_insert(c: &mut Criterion) {
-    println!("Running bench_insert");
-    c.bench_function("insert 1000 keys", |b| {
-        b.iter(|| {
-        let mut store = Store::new();
-            for i in 0..1000 {
-                store.insert(format!("key-{}", i).as_str(), "value").unwrap();
-            }
-        })
-    });
-}
-
-pub fn bench_get(c: &mut Criterion) {
-    println!("Running bench_get");
-    c.bench_function("get 1000 keys", |b| {
-        let mut store = Store::new();
-        for i in 0..1000 {
-            store.insert(format!("key{}", i).as_str(), "value").unwrap();
+pub fn setup_test_store(db_name: &str) -> Store {
+    let mut store = Store::new();
+    let start = SystemTime::now();
+    let since_the_epoch = start
+        .duration_since(UNIX_EPOCH)
+        .expect("Time went backwards");
+    let timestamp = since_the_epoch.as_secs();
+    let db_name = format!("db-test-bench-{}-{}", db_name, timestamp);
+    match STORAGE_MUTEX
+        .lock()
+        .unwrap()
+        .load_file(Some(db_name.as_str()))
+    {
+        Ok(data) => store.data = data,
+        Err(err) => {
+            println!("Invalid: {}", err);
         }
+    }
+    store
+}
+
+pub fn bench_store_crud(c: &mut Criterion) {
+    let mut group = c.benchmark_group("[Store persistent CRUD benchamrks");
+    group.sample_size(10);
+    let mut store = setup_test_store("all");
+    let mut counter = 0;
+
+    group.bench_function("insert 1000 keys", |b| {
         b.iter(|| {
             for i in 0..1000 {
-                let _ = store.get(&format!("key{}", i));
+                let fresh_key = format!("b-{}-key-{}", counter, i);
+                store
+                    .insert(
+                        &fresh_key,
+                        &format!("value-{}", i),
+                    )
+                    .unwrap();
             }
+            counter += 1;
+        })
+    });
+    counter = 0;
+    group.bench_function("get 1000 keys", |b| {
+        b.iter(|| {
+            for i in 0..1000 {
+                let fresh_key = format!("b-{}-key-{}", counter, i);
+                let _ = store.get(&fresh_key);
+            }
+            counter += 1;
+        })
+    });
+    counter = 0;
+    group.bench_function("update 1000 keys", |b| {
+        b.iter(|| {
+            for i in 0..1000 {
+                let fresh_key = format!("b-{}-key-{}", counter, i);
+                store.update(&fresh_key, "new_value").unwrap();
+            }
+            counter += 1;
+        })
+    });
+    counter = 0;
+    group.bench_function("delete 1000 keys", |b| {
+        b.iter(|| {
+            for i in 0..1000 {
+                let fresh_key = format!("b-{}-key-{}", counter, i);
+                store.delete(&fresh_key);
+            }
+            counter += 1;
         })
     });
 }
 
-pub fn bench_update(c: &mut Criterion) {
-    c.bench_function("update 1000 keys", |b| {
-        let mut store = Store::new();
-        for i in 0..1000 {
-            store.insert(format!("key{}", i).as_str(), "value").unwrap();
-        }
-        b.iter(|| {
-            for i in 0..1000 {
-                store.update(&format!("key{}", i), "new_value").unwrap();
-            }
-        })
-    });
-}
-
-pub fn bench_delete(c: &mut Criterion) {
-    c.bench_function("delete 1000 keys", |b| {
-        let mut store = Store::new();
-        for i in 0..1000 {
-            store.insert(format!("key{}", i).as_str(), "value").unwrap();
-        }
-        b.iter(|| {
-            for i in 0..1000 {
-                store.delete(&format!("key{}", i));
-            }
-        })
-    });
-}
-
-criterion_group!(benches, bench_insert, bench_get, bench_update, bench_delete);
+criterion_group!(benches, bench_store_crud);
 criterion_main!(benches);
